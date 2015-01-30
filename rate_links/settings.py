473a9ec598375939944dd825c6b60f481c78d4b8
solean <he15man2007@gmail.com>
2015-01-30 00:11:55 -0500

    Contact info and settings change

--- rate_links/settings.py
+++ rate_links/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for rate_links project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -57,13 +47,6 @@ WSGI_APPLICATION = 'rate_links.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-# DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.sqlite3',
-#           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#       }
-# }
 
 DATABASES = {
     'default': {
