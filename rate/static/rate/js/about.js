8d8a0ab1889c6eb1e82b78c62eb27ca4d9102abc
solean <he15man2007@gmail.com>
2015-06-06 00:47:05 -0400

    About page updated with redesign

---
+++ rate/static/rate/js/about.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#website-button').click(function() {
+		window.location = 'http://chrisschnabel.com';
+	})
+
+	$('#email-button').click(function() {
+		window.location = 'mailto:chrisjschnabel@gmail.com';
+	});
+
+	$('#github-button').click(function() {
+		window.location = 'https://github.com/solean';
+	});
+});
